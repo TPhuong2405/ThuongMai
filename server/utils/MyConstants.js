4da7bbe94d4f86cffed6b56931dde3de15d455ff
TPhuong2405 <phuongle.240503@gmail.com>
2023-11-28 13:28:09 +0700

    init

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'cluster0.odjerdm.mongodb.net', //lấy trc @
+    DB_USER: 'thanhphuong240503', 
+    DB_PASS: 'Phuongpewpew01',
+    DB_DATABASE: 'shoppingonline',
+    JWT_SECRET: 'Phuongpewpew',
+    JWT_EXPIRES: '36000000', // in milliseconds
+    EMAIL_USER: 'phuongle.240503@gmail.com', // gmail service
+    EMAIL_PASS: 'qufpeotvfjlaqhyz'
+  };
+  module.exports = MyConstants;
